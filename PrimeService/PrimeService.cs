4d4938bb9d655ab28a278b4bd0a83ca1826b9529
shariedong <41610654+shariedong@users.noreply.github.com>
2020-11-10 15:41:29 +1300

    first commit

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            
+        throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
